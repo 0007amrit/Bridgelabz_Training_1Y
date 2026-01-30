72095ea4fec32dc46a2b6c8587588b85c6f331ca
Amrit Mishra <0007amrit@gmail.com>
2026-01-30 11:48:19 +0530

    Clean branch: keep only Basic Programming

--- Programming_Construct/Circle.java
+++
@@ -1,9 +0,0 @@
-public class Circle {
-    public static void main(String arg[])
-    {
-        int r=5;
-        float PI= 22/7;
-        float Area = PI*r*r;
-        System.out.println("Circle of Area : "+Area);
-    }    
-}
