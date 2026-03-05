88d2147f196694ac882466ac645ea30c4610e5be
Amrit Mishra <0007amrit@gmail.com>
2026-03-05 17:03:49 +0530

    Clean main branch, keep only README

--- Basic_Programming_1/Sum.java
+++
@@ -1,8 +0,0 @@
-class Sum{
- 
- public static void main(String arg[]) {
-     int a=12;
-     int b=25;
-     int sum =a+b; 
-     System.out.print("Addition of two number :-" +sum);
- }}
--- Programming_Construct/Add.java
+++
@@ -1,9 +0,0 @@
-  public class Add {
-    public static void main(String arg[]){
-        int a =10;
-        int b =25;
-        int add = a+b;
-        System.out.println("Add of two no.= "+add);
-    }
-    
-}
