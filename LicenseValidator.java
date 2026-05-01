9483c14376aa7e29dba8bf1710f2201a5b292e60
Amrit Mishra <0007amrit@gmail.com>
2026-05-01 23:26:53 +0530

    Done with some codes

--- LicenseValidator.java
+++ LicenseValidator.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class LicenseValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter License Plate: ");
+        String plate = sc.nextLine();
+
+
+        String regex = "^[A-Z]{2}[0-9]{4}$";
+
+        if (plate.matches(regex)) {
+            System.out.println(" Valid License Plate");
+        } else {
+            System.out.println(" Invalid License Plate");
+        }
+
+        sc.close();
+    }
 }
